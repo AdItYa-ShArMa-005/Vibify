3c0dffad30670136367b3eeed70c4560f0237f82
AdItYa-ShArMa-005 <14sharmaaditya2004@gamil.com>
2026-04-15 18:56:49 +0530

    more changes

--- src/components/Artists.jsx
+++ src/components/Artists.jsx
@@ -1,4 +1,30 @@
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+
 const Artists=()=>{
+    const songPool = useSelector(state => state.songPool.value);
+    const[artists,setArtists] = useState([]);
+
+    useEffect(() => {
+        setArtists(...artists,songPool.map(song => {
+             let list =  song.singers.map(item => {
+                if(!artists.includes(item))
+                    return item;
+             } );
+             console.log("-------------"+list);
+             return list;
+        }));
+    },[]);
+
+    useEffect(() => {
+        setArtists(songPool.map(song => {
+             return song.singers.map(item => {
+                if(!artists.includes(item))
+                    return item;
+             } );
+        }));
+    },[songPool]);
+    console.log(artists.slice(0,11));
     return(
         <div>
             This is artists section
