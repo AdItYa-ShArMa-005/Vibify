7a65f5e9fc65c12ecde7690065e5335984cc53f7
ANGEL TRIPATHI <angel@ANGELs-MacBook-Air.local>
2026-04-15 19:19:18 +0530

    library changes done

--- src/components/Library.jsx
+++ src/components/Library.jsx
@@ -1,9 +1,23 @@
+import { useSelector } from "react-redux";
+
+import LibrarySongCard from "../components/LibrarySongCard";
+
 const Library = () => {
+    const songs = useSelector((state) => state.songPool.value);
+
     return (
         <div className="library">
             <div className="library-header">
                 <h2>Your Library</h2>
                 <p>All your saved songs, playlists, and albums in one place.</p>
+            </div>
+
+            <div className="songs">
+                {
+                   songs.map((song,index)=>(
+                      <LibrarySongCard key={index} index={index} song={song}/>
+                   ))
+                }
             </div>
         </div>
     )
